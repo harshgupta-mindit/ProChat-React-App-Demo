fc1f21c97d69d42e7333e2a5a0a2e495619a9900
harshgupta-mindit <harsh.gupta@minditsystems.com>
2023-09-20 09:54:58 +0530

    Card component added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
-
+import { Routes, Route } from "react-router-dom";
 
 import NavbarMenu from './components/Navbar/Navbar';
+import Error from './pages/Error/Error';
+import Login from './pages/Login/Login';
+import SignUp from './pages/Signup/SignUp';
+import Products from './pages/products/Products';
 
 import Home from './pages/home/Home';
 
@@ -11,9 +15,15 @@ function App() {
     <div className="App">
 
       <NavbarMenu />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/products' element={<Products />} />
 
-      <Home/>
-      
+        <Route path='*' element={<Error />} />
+      </Routes>
+
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import "../../styles/Navbar.css"
+import { Link } from 'react-router-dom'
 const NavbarMenu = () => {
     return (
         <>
             <nav className='nav'>
                 <h1 className='nav-h1'>Pro Shop <sub className='nav-sub'>by Mind IT</sub></h1>
             
-                <ul className="nav-ul">
-                    <li className="nav-li">Products</li>
-                    <li className="nav-li">Log in</li>
-                    <li className="nav-li">Sign Up</li>
-                </ul>
+                <div className="nav-ul">
+                    <Link style={{textDecoration:'none'}} to="/products"><li className="nav-li">Products</li></Link>
+                    <Link style={{textDecoration:'none'}} to="/login"><li className="nav-li">Log in</li></Link>
+                    <Link style={{textDecoration:'none'}} to='signup'><li className="nav-li">Sign Up</li></Link>
+                </div>
             
             </nav>
         </>
