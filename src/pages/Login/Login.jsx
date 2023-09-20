b156b93bcdfd2f03215b0f01778795e498360e01
harshgupta-mindit <harsh.gupta@minditsystems.com>
2023-09-20 10:44:59 +0530

    login screen added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
-
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import "./Login.css"
 import { Link } from 'react-router-dom';
+
+import * as Yup from "yup";
 
 const Login = () => {
   return (
@@ -10,17 +12,31 @@ const Login = () => {
         <div className="login-body">
           <h1 className='login-h1'>Log-in</h1>
 
-          <form className='login-form' action="">
-            <label className='login-email-label' htmlFor=""></label>
-            <input className='login-email-input' type="text" placeholder='email' />
-            <label className='login-password-label' htmlFor=""></label>
-            <input className='login-password-input' type="text" placeholder='password' />
+          <Formik
+            initialValues={{
+              email: "",
+              password: ""
+            }}
 
-            <input className='login-submit-btn' type="submit" value="Login" />
-          </form>
+            validationSchema={Yup.object({
+              email: Yup.string().email("Must be a valid email").required("Email is required"),
+              password: Yup.string().required("Password is required").min(8,"Must be greater than 8 characters"),
+            })}
 
+            onSubmit={async (values) => {
+              console.log(values);
+            }}
+          >
+            <Form className='login-form'>
+              <Field className='login-email-input' type="email" name="email" placeholder="Enter Email" />
+              <ErrorMessage style={{color:"#D80032"}} component="label" name="email" />
+              <Field className='login-password-input' type="password" name="password" placeholder="Enter Password" />
+              <ErrorMessage style={{color:"#D80032"}} component="label" name="password" />
 
-          <p style={{textAlign:'center', marginTop:"30px"}}>Don't have an account ? <Link to='/register' style={{color:'#D80032'}} >Register</Link></p>
+              <button className='login-submit-btn' type="Submit">Log In</button>
+            </Form>
+          </Formik>
+          <p style={{ textAlign: 'center', marginTop: "30px" }}>Don't have an account ? <Link to='/register' style={{ color: '#D80032' }} >Sign Up</Link></p>
 
         </div>
       </div>
